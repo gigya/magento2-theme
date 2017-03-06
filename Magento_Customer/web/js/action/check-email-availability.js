0abbe75854ad08c746abfdd5c86c1f21ef25592e
luciodiri <guy.avivi@gigya-inc.com>
2017-03-06 10:42:53 +0200

    checkout - open login screen when user email found. + add registration screen to checkout success page

---
+++ Magento_Customer/web/js/action/check-email-availability.js
@@ -0,0 +1,42 @@
+/**
+ * Override Magento customer check email availability action, for step 1 - email verification in checkout page.
+ * If the customer email exists, open gigya login popup.
+ * Copyright © 2016 Magento. All rights reserved.
+ * See COPYING.txt for license details.
+ * Override onpage checkout page email availability test.
+ * If email exists in Magento show the Gigya login screen
+ */
+define(
+    [
+        'mage/storage',
+        'Magento_Checkout/js/model/url-builder'
+    ],
+    function (storage, urlBuilder) {
+        'use strict';
+
+        return function (deferred, email) {
+            return storage.post(
+                urlBuilder.createUrl('/customers/isEmailAvailable', {}),
+                JSON.stringify({
+                    customerEmail: email
+                }),
+                false
+            ).done(
+                function (isEmailAvailable) {
+                    if (isEmailAvailable) {
+                        deferred.resolve();
+                    } else {
+                        console.log("Gigya theme checkout override");
+                        jQuery("#gigya-login-popup").modal("openModal");
+                        // deferred.reject();
+                    }
+                }
+            ).fail(
+                function () {
+                    deferred.reject();
+                }
+            );
+        };
+    }
+);
+
